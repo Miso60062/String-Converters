df5b3e6ae496f40e7d679bd2182ee121501f32d4
Miso60062 <miso60062@gmail.com>
2021-09-01 21:27:16 +0200

    String-Converters

---
+++ script.js
@@ -0,0 +1,48 @@
+const ascii = document.getElementById('ascii')
+const binary = document.getElementById('binary')
+const hexadecimal = document.getElementById('hexadecimal')
+const octal = document.getElementById('octal')
+const output = document.getElementById('output')
+
+document.querySelector('.dark-light').addEventListener
+('click', () => {
+    document.body.classList.toggle('dark-light');
+})  
+
+    binary.addEventListener('click', () => {
+        const input = document.getElementById('text').value
+        output.value = ""
+        for(let i = 0; i < input.length; i++) {
+        output.value += input[i].charCodeAt(0).toString(2).substring(1) + " "
+        }
+})
+
+    ascii.addEventListener('click', () => {
+        const input = document.getElementById('text').value
+        output.value = ""
+        for(let i = 0; i < input.length; i++) {
+            output.value += input[i].charCodeAt(0) + "  "
+        }
+    })
+
+
+    hexadecimal.addEventListener('click', () => {
+        const input = document.getElementById('text').value
+        output.value = ""
+        for(let i = 0; i < input.length; i++) {
+            output.value += input[i].charCodeAt(0).toString(16) + " "
+        }
+    })
+
+
+    octal.addEventListener('click', () => {
+        const input = document.getElementById('text').value
+        output.value = ""
+        for(let i = 0; i < input.length; i++) {
+            output.value += input[i].charCodeAt(0).toString(8) + " "
+        }
+    })
+
+
+
+
